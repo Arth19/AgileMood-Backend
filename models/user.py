5b6f5a89f3ce0e9c8972243a12edfacf6b3a04d0
Jurupoc <jurupoc222@gmail.com>
2025-02-09 12:13:06 -0300

    Removing unncessary relationship, deleting DB file.

--- models/user.py
+++ models/user.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 import databases.sqlite_data_provider as db
 
 
@@ -9,5 +8,3 @@ class User(db.Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
-
-    records = relationship("EmotionRecord", back_populates="user")
